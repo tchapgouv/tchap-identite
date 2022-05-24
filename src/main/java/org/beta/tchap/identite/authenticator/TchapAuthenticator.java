5a71081e04ba10e094b5d2bce1b1a5df436d8f97
Olivier D <odelcroi@gmail.com>
2022-05-24 16:33:04 +0200

    propose an error page (#27)
    * feat: update dsfr to last version 1.5.1
    
    * feat: add template for unauthorized user

--- src/main/java/org/beta/tchap/identite/authenticator/TchapAuthenticator.java
+++ src/main/java/org/beta/tchap/identite/authenticator/TchapAuthenticator.java
@@ -13,12 +13,15 @@ import org.keycloak.models.UserModel;
 import org.keycloak.protocol.oidc.OIDCLoginProtocol;
 import org.keycloak.sessions.AuthenticationSessionModel;
 
+import javax.ws.rs.core.Response;
+
 import static org.beta.tchap.identite.authenticator.OtpLoginAuthenticator.*;
 
 public class TchapAuthenticator implements Authenticator {
 
     private final SecureCode secureCode;
     private final EmailSender emailSender;
+    private static final String FTL_UNAUTHORIZED_USER       = "unauthorized-user.ftl";
 
     private static final Logger LOG = Logger.getLogger(TchapAuthenticator.class);
 
@@ -32,20 +35,37 @@ public class TchapAuthenticator implements Authenticator {
         AuthenticationSessionModel session  = context.getAuthenticationSession();
         String loginHint = session.getClientNote(OIDCLoginProtocol.LOGIN_HINT_PARAM);
 
-        if(loginHint !=null){
-            LOG.infof("Authenticate login : %s", loginHint);
-            context.getAuthenticationSession().setAuthNote(AUTH_NOTE_USER_EMAIL, loginHint);
-            UserModel user = getUser(context);
-
-            if (user == null || !user.isEnabled()) {
-                context.failure(AuthenticationFlowError.INVALID_USER);
-            } else {
-                generateAndSendCode(context);
-                context.success();
-            }
-        }else{
-            context.failure(AuthenticationFlowError.INVALID_USER);
+        if(loginHint ==null){
+            context.failure(AuthenticationFlowError.GENERIC_AUTHENTICATION_ERROR,
+                    Response.status(400).build(), "request is malformed", "request is malformed" );
+            return;
         }
+
+        LOG.infof("Authenticate login : %s", loginHint);
+        context.getAuthenticationSession().setAuthNote(AUTH_NOTE_USER_EMAIL, loginHint);
+        UserModel user = getUser(context);
+
+        if (user == null) {
+            showUnautorhizedUser(context);
+            return;
+        }
+
+        //user has been found
+        generateAndSendCode(context);
+        context.success();
+    }
+
+    private void showUnautorhizedUser(AuthenticationFlowContext context){
+        String message = "Cet email ne correspond pas à une agence de l'État. Si vous appartenez à un service de l'État mais votre email n'est pas reconnu par AudioConf, contactez-nous pour que nous le rajoutions!";
+           /* context.failure(AuthenticationFlowError.UNKNOWN_USER, Response.status(401).build(),
+                    "user was not found in Tchap", message );
+            */
+        context.failure(AuthenticationFlowError.GENERIC_AUTHENTICATION_ERROR,  context.form()
+                .createForm(FTL_UNAUTHORIZED_USER)
+                , "unknown user", "unknow user");
+
+
+
     }
 
     private void generateAndSendCode(AuthenticationFlowContext context)
@@ -69,7 +89,8 @@ public class TchapAuthenticator implements Authenticator {
 
     private UserModel getUser(AuthenticationFlowContext context)
     {
-        return context.getSession().users().getUserByEmail(context.getRealm(), context.getAuthenticationSession().getAuthNote(AUTH_NOTE_USER_EMAIL));
+        return context.getSession().users().getUserByEmail(context.getRealm(),
+                context.getAuthenticationSession().getAuthNote(AUTH_NOTE_USER_EMAIL));
     }
 
 
---
+++ theme/tchap-otp-login/login/unauthorized-user.ftl
@@ -0,0 +1,18 @@
+<#import "template.ftl" as layout>
+<@layout.registrationLayout displayInfo=social.displayInfo pageTitle="${client.name}" clientUrl="${client.baseUrl}" ; section>
+    <#if section = "title">
+        ${msg("loginTitle",(realm.displayName!''))}
+    <#elseif section = "header">
+    <#elseif section = "form">
+        <h2>Réserver une conférence téléphonique</h2>
+        <div class="fr-callout fr-alert fr-alert--warning">
+
+            <p class="fr-callout__text">Cet email ne correspond pas à une agence de l'État. Si vous appartenez à un service de l'État mais votre email n'est pas reconnu par AudioConf, contactez-nous pour que nous le rajoutions!</p>
+
+            <a class="fr-btn fr-btn--primary" title="Nous contacter" href="https://audioconf.numerique.gouv.fr/contact">Nous contacter</a>
+
+
+        </div>
+        <a type="submit" class="fr-btn" href="${client.baseUrl}">Réserver une autre conférence</a>
+    </#if>
+</@layout.registrationLayout>
