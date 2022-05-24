66aee8af43bebf78da3cc3cb91415798a32a6028
mcalinghee <mcalinghee.dev@gmail.com>
2022-05-24 16:56:14 +0200

    add Constants for env vars

---
+++ src/main/java/org/beta/tchap/identite/utils/Constants.java
@@ -0,0 +1,8 @@
+package org.beta.tchap.identite.utils;
+
+public interface Constants {
+
+    String TCHAP_ACCOUNT = "TCHAP_ACCOUNT";
+    String TCHAP_PASSWORD = "TCHAP_PASSWORD";
+    String TCHAP_HOME_SERVER_LIST = "TCHAP_HOME_SERVER_LIST";
+}
