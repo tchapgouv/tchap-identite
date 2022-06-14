82549a5a8b19aa03677dcba086e63e5cbfa1d196
Calev Eliacheff <c.eliacheff@gmail.com>
2022-06-14 13:55:51 +0200

    More TUs

--- src/test/java/org/beta/tchap/identite/bot/testing/FakeRoomClient.java
+++ src/test/java/org/beta/tchap/identite/bot/testing/FakeRoomClient.java
@@ -5,10 +5,13 @@ import org.beta.tchap.identite.matrix.rest.room.RoomClient;
 import org.beta.tchap.identite.matrix.rest.room.SendMessageBody;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 class FakeRoomClient implements RoomClient {
     public Map<String, ArrayList<String>> rooms;
+    public String lastMessage = null;
 
     @Override
     public Map<String, ArrayList<String>> listDMRooms(String userId) {
@@ -17,14 +20,22 @@ class FakeRoomClient implements RoomClient {
 
     @Override
     public void updateDMRoomList(String userId, Map<String, ArrayList<String>> dMRoomsList) {
-
+        this.rooms = dMRoomsList;
     }
 
     @Override
     public Map<String, String> createDM(CreateDMBody createDMBody) {
-        return null;
+        String invitedId = createDMBody.getInvite().get(0);
+        String roomId = "123";
+        this.rooms.put(invitedId, new ArrayList<>(Collections.singleton(roomId)));
+
+        HashMap<String, String> returnValue = new HashMap<>();
+        returnValue.put("room_id", roomId);
+        return returnValue;
     }
 
     @Override
-    public void sendMessage(String roomId, SendMessageBody messageBody) {}
+    public void sendMessage(String roomId, SendMessageBody messageBody) {
+        lastMessage = messageBody.getBody();
+    }
 }
