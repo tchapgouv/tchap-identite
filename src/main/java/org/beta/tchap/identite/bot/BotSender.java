01221e59474acf0eac1259ad4632512f468130c4
Calev Eliacheff <c.eliacheff@gmail.com>
2022-06-15 14:25:33 +0200

    Fix int tests

--- src/main/java/org/beta/tchap/identite/bot/BotSender.java
+++ src/main/java/org/beta/tchap/identite/bot/BotSender.java
@@ -1,6 +1,14 @@
 package org.beta.tchap.identite.bot;
 
+import org.beta.tchap.identite.matrix.rest.homeserver.HomeServerService;
+import org.beta.tchap.identite.matrix.rest.login.LoginService;
 import org.beta.tchap.identite.matrix.rest.room.RoomService;
+import org.beta.tchap.identite.matrix.rest.user.UserInfoResource;
+import org.beta.tchap.identite.matrix.rest.user.UserService;
+import org.beta.tchap.identite.utils.Constants;
+import org.beta.tchap.identite.utils.Environment;
+
+import static org.beta.tchap.identite.matrix.rest.homeserver.HomeServerService.buildHomeServerUrl;
 
 public class BotSender {
 
@@ -10,8 +18,19 @@ public class BotSender {
         this.roomService = roomService;
     }
 
-    public void sendOtp(String otp, String destMatrixId) {
-        String roomId = roomService.createDM(destMatrixId);
+    public String openDM(String destMatrixId) {
+        return roomService.createDM(destMatrixId);
+    }
+
+    public void sendMessage(String roomId, String otp) {
+        roomService.sendMessage(roomId, "Bonjour, voici ton OTP !");
         roomService.sendMessage(roomId, otp);
     }
+
+
+//    public void sendOtp(String otp, String email) {
+//        String roomId = roomService.createDM(destMatrixId.getUserId());
+//        roomService.sendMessage(roomId, "Bonjour, voici ton OTP !");
+//        roomService.sendMessage(roomId, otp);
+//    }
 }
--- src/main/java/org/beta/tchap/identite/bot/BotSenderFactory.java
+++ src/main/java/org/beta/tchap/identite/bot/BotSenderFactory.java
@@ -1,29 +1,14 @@
 package org.beta.tchap.identite.bot;
 
-import org.beta.tchap.identite.matrix.rest.homeserver.HomeServerService;
-import org.beta.tchap.identite.matrix.rest.login.LoginService;
 import org.beta.tchap.identite.matrix.rest.room.RoomClient;
 import org.beta.tchap.identite.matrix.rest.room.RoomClientFactory;
 import org.beta.tchap.identite.matrix.rest.room.RoomService;
-import org.beta.tchap.identite.utils.Constants;
-import org.beta.tchap.identite.utils.Environment;
-
-import static org.beta.tchap.identite.matrix.rest.homeserver.HomeServerService.buildHomeServerUrl;
 
 public class BotSenderFactory {
     private static BotSender instance;
 
-    public static BotSender getInstance() {
+    public static BotSender getInstance(String accountHomeServerUrl, String accessToken) {
         if (instance == null) {
-            String account = Environment.getenv(Constants.TCHAP_ACCOUNT);
-            String password = Environment.getenv(Constants.TCHAP_PASSWORD);
-
-            HomeServerService homeServerService = new HomeServerService();
-            LoginService loginService = new LoginService();
-
-            String accountHomeServerUrl = buildHomeServerUrl(homeServerService.findHomeServerByEmail(account));
-            String accessToken = loginService.findAccessToken(accountHomeServerUrl, account, password);
-
             RoomClient roomClient = RoomClientFactory.build(accountHomeServerUrl, accessToken);
             RoomService roomService = new RoomService(roomClient);
             instance = new BotSender(roomService);
