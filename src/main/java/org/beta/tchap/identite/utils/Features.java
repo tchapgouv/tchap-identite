6414ecfaa1905a790629352a0f4338384acc12a7
celiacheff <calev.eliacheff@beta.gouv.fr>
2022-06-27 14:22:19 +0200

    Feat/bot flag (#101)
    * template formatting
    
    * add flag in template and code
    
    * Move flag to external class
    
    * Missing var

---
+++ src/main/java/org/beta/tchap/identite/utils/Features.java
@@ -0,0 +1,7 @@
+package org.beta.tchap.identite.utils;
+
+public class Features {
+    public static boolean isTchapBotEnabled() {
+        return Boolean.parseBoolean(Environment.getenv(Constants.FEATURE_TCHAP_BOT_OTP));
+    }
+}
