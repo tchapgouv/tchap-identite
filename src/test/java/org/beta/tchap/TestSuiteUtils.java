ca37566d9e797da40db8e412f28252e69e489234
mcalinghee <mcalinghee.dev@gmail.com>
2022-07-06 19:42:50 +0200

    initiate all services once before testing

--- src/test/java/org/beta/tchap/TestSuiteUtils.java
+++ src/test/java/org/beta/tchap/TestSuiteUtils.java
@@ -5,7 +5,7 @@ import io.github.cdimascio.dotenv.Dotenv;
 public class TestSuiteUtils {
 
     public static final String ENV_DELETE_ROOM_AFTER_TESTS = "TEST_DELETE_ROOM_AFTER_TESTS";
-    public static String ENV_TEST_BOT_TO_USER_MID = "TEST_BOT_TO_USER_MID";
+    public static String TEST_BOT_TO_USER_MID = "TEST_BOT_TO_USER_MID";
     public static String TEST_USER2_ACCOUNT = "TEST_USER2_ACCOUNT";
     public static String TEST_USER2_PASSWORD = "TEST_USER2_PASSWORD";
     public static String TEST_USER2_MATRIXID = "TEST_USER2_MATRIXID";
